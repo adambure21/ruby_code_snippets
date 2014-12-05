f596a4db7d32a76884061e69aa98e29778fc7c66
adambure21 <adambure21@gmail.com>
2014-12-05 08:15:53 +0530

    String capitalize and delimiters are added

---
+++ string_capitalize.rb
@@ -0,0 +1,9 @@
+print "Enter name : \t "
+name = gets
+print "Inspection gives #{name.object_id}\n"
+
+print "Entered name is #{name}\n"
+name = name.capitalize!
+
+
+print "Inspection gives #{name.object_id}\n"
---
+++ string_delimiter.rb
@@ -0,0 +1,11 @@
+# The Other string delimiters can be used
+# are %!-----!
+# %Q!----!
+# %q!----!
+# %/-----/
+
+name = "Abhinay"
+puts %Q/ABHinay Ambure/
+puts %q!My name is Abhinay!
+puts %/I am Computer Engineer/
+puts %!Computer Science is my branch!
